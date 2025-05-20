7ee1ee2e72932344dfe81cabda25b7642f4d17eb
nickAnhel <anhimovn1@gmail.com>
2025-05-20 17:37:22 +0300

    Update admin panel

--- server/src/admin/admin.py
+++ server/src/admin/admin.py
@@ -1,7 +1,15 @@
 from sqladmin import Admin
 
 from src.admin.auth import AdminAuth
-from src.admin.views import PostAdminView, SessionAdminView, UserAdminView
+from src.admin.views import (
+    ChatAdminView,
+    EventAdminView,
+    MembershipAdminView,
+    MessageAdminView,
+    PostAdminView,
+    SessionAdminView,
+    UserAdminView,
+)
 from src.config import settings
 from src.database import async_engine
 
@@ -15,6 +23,10 @@ def create_admin(app) -> Admin:
 
     admin.add_model_view(UserAdminView)
     admin.add_model_view(PostAdminView)
+    admin.add_model_view(ChatAdminView)
+    admin.add_model_view(MembershipAdminView)
+    admin.add_model_view(MessageAdminView)
+    admin.add_model_view(EventAdminView)
     admin.add_model_view(SessionAdminView)
 
     return admin
--- server/src/admin/views.py
+++ server/src/admin/views.py
@@ -1,12 +1,15 @@
 from sqladmin import ModelView
 
 from src.admin.models import SessionModel
+from src.chats.models import ChatModel, MembershipModel
+from src.events.models import EventModel
+from src.messages.models import MessageModel
 from src.posts.models import PostModel
 from src.users.models import UserModel
 
 
 class UserAdminView(ModelView, model=UserModel):
-    column_list = ["user_id", "username", "is_admin"]
+    column_list = ["user_id", "username", "is_admin", "subscribers_count"]
     column_searchable_list = ["user_id", "username"]
     column_details_exclude_list = ["hashed_password"]
 
@@ -25,4 +28,23 @@ class PostAdminView(ModelView, model=PostModel):
 
 class SessionAdminView(ModelView, model=SessionModel):
     column_list = ["session_id", "user_id", "issued_at", "expires_at"]
-    column_sortable_list = ["issued_at", "expires_at"]
+    column_sortable_list = ["session_id", "issued_at", "expires_at"]
+
+
+class ChatAdminView(ModelView, model=ChatModel):
+    column_list = ["chat_id", "title", "is_private", "owner_id"]
+    column_searchable_list = ["chat_id", "title"]
+
+
+class MembershipAdminView(ModelView, model=MembershipModel):
+    column_list = ["chat_id", "user_id"]
+
+
+class MessageAdminView(ModelView, model=MessageModel):
+    column_list = ["message_id", "chat_id", "content_ellipsis"]
+    column_searchable_list = ["messag_id", "content"]
+
+
+class EventAdminView(ModelView, model=EventModel):
+    column_list = ["event_id", "chat_id", "user_id", "event_type", "created_at"]
+    column_searchable_list = ["event_id", "event_type"]
